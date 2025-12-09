17cd9fac09012ee66f8827c8c579920c1a104dc6
Wanderson Soares <wandersonsoares841@gmail.com>
2025-12-09 09:45:26 -0300

    corrige env

--- gateway/run.php
+++ gateway/run.php
@@ -1,11 +1,14 @@
 <?php
 
+use Gateway\Gateway;
+
 require __DIR__ . '/vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
 $dotenv->load();
 
 require_once __DIR__ . "/src/Gateway.php";
 
 $gateway = new Gateway();
+
 $gateway->start();
